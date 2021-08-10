59910ee1f1012548d050d8aa34568353636c4e2e
hasegawa <hasegawa@localhost>
2021-08-10 22:32:40 +0900

    Make ByteBuffer as separate file.

---
+++ src/util/ByteBuffer.cpp
@@ -0,0 +1,237 @@
+/*******************************************************************************
+ * Copyright (c) 2021, Yasuhiro Hasegawa
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+
+
+//
+// ByteBuffer.cpp
+//
+
+#include "Util.h"
+static const Logger logger = Logger::getLogger("bytebuffer");
+
+#include "ByteBuffer.h"
+
+
+void ByteBuffer::copyFrom(int len, const quint8* data) {
+	if (len < 0) {
+		logger.error("Too large len");
+		logger.error("  len      = %5d", len);
+		ERROR();
+	}
+	if (myCapacity < len) {
+		logger.error("Exceed capacity");
+		logger.error("  capacity = %5d", myCapacity);
+		logger.error("  len      = %5d", len);
+		ERROR();
+	}
+	// clear myData with zero
+	memset(myData, 0, (size_t)myCapacity);
+	// copy from data to myData
+	memcpy(myData, data, (size_t)len);
+	// reset position and limit
+	myPosition = 0;
+	myLimit    = len;
+}
+
+void ByteBuffer::position(int newValue	) {
+	if (0 <= newValue && newValue <= myLimit) {
+		myPosition = newValue;
+	} else {
+		logger.error("Exceed limit");
+		logger.error("  newValue = %5d", newValue);
+		logger.error("  limit    = %5d", myLimit);
+		ERROR();
+	}
+}
+
+void ByteBuffer::mark() {
+	if (myMarkPos == INVALID_POS) {
+		myMarkPos = myPosition;
+	} else {
+		logger.error("Unexpected");
+		logger.error("  myMarkPos = %d", myMarkPos);
+		ERROR();
+	}
+}
+void ByteBuffer::reset() {
+	if (myMarkPos == INVALID_POS) {
+		logger.error("Unexpected");
+		ERROR();
+	} else {
+		myPosition = myMarkPos;
+		myMarkPos = INVALID_POS;
+	}
+}
+
+
+//
+// ByteBuffer::read
+//
+void ByteBuffer::read8(const int index, quint8& value) {
+	const int readSize = 1;
+	if ((index + readSize) <= myLimit) {
+		const quint8* data = myData + index;
+		value = data[0];
+	} else {
+		logger.error("Exceed limit");
+		logger.error("  index    = %5d", index);
+		logger.error("  readSize = %5d", readSize);
+		logger.error("  limit    = %5d", myLimit);
+		ERROR();
+	}
+}
+void ByteBuffer::read16(const int index, quint16& value) {
+	const int readSize = 2;
+	if ((index + readSize) <= myLimit) {
+		const quint8* data = myData + index;
+		value = (data[0] << 8) | (data[1] << 0);
+	} else {
+		logger.error("Exceed limit");
+		logger.error("  index    = %5d", index);
+		logger.error("  readSize = %5d", readSize);
+		logger.error("  limit    = %5d", myLimit);
+		ERROR();
+	}
+}
+void ByteBuffer::read32(const int index, quint32& value) {
+	const int readSize = 4;
+	if ((index + readSize) <= myLimit) {
+		const quint8* data = myData + index;
+		value = (data[0] << 24) | (data[1] << 16) | (data[2] << 8) | (data[3] << 0);
+	} else {
+		logger.error("Exceed limit");
+		logger.error("  index    = %5d", index);
+		logger.error("  readSize = %5d", readSize);
+		logger.error("  limit    = %5d", myLimit);
+		ERROR();
+	}
+}
+void ByteBuffer::read48(const int index, quint64& value) {
+	const int readSize = 6;
+	if ((index + readSize) <= myLimit) {
+		const quint8* data = myData + index;
+		value = ((quint64)data[0] << 40) | ((quint64)data[1] << 32) |
+				((quint64)data[2] << 24) | ((quint64)data[3] << 16) |
+				((quint64)data[4] <<  8) | ((quint64)data[5] <<  0);
+	} else {
+		logger.error("Exceed limit");
+		logger.error("  index    = %5d", index);
+		logger.error("  readSize = %5d", readSize);
+		logger.error("  limit    = %5d", myLimit);
+		ERROR();
+	}
+}
+void ByteBuffer::read(const int index, const int readSize, quint8* value) {
+	if ((index + readSize) <= myLimit) {
+		const quint8* data = myData + index;
+		memcpy(value, data, readSize);
+	} else {
+		logger.error("Exceed limit");
+		logger.error("  index    = %5d", index);
+		logger.error("  readSize = %5d", readSize);
+		logger.error("  limit    = %5d", myLimit);
+		ERROR();
+	}
+}
+
+
+//
+// ByteBuffer::write
+//
+void ByteBuffer::write8(const int index, quint8 value) {
+	const int writeSize = 1;
+	if (myCapacity < (index + writeSize)) {
+		logger.error("Exceed capacity");
+		logger.error("  capacity  = %5d", myCapacity);
+		logger.error("  index     = %5d", index);
+		logger.error("  writeSize = %5d", writeSize);
+		ERROR();
+	}
+	quint8* data = myData + index;
+	data[0] = value;
+}
+void ByteBuffer::write16(const int index, quint16 value) {
+	const int writeSize = 2;
+	if (myCapacity < (index + writeSize)) {
+		logger.error("Exceed capacity");
+		logger.error("  capacity  = %5d", myCapacity);
+		logger.error("  index     = %5d", index);
+		logger.error("  writeSize = %5d", writeSize);
+		ERROR();
+	}
+	quint8* data = myData + index;
+	data[0] = (quint8)(value >> 8);
+	data[1] = (quint8)(value);
+}
+void ByteBuffer::write32(const int index, quint32 value) {
+	const int writeSize = 4;
+	if (myCapacity < (index + writeSize)) {
+		logger.error("Exceed capacity");
+		logger.error("  position = %5d", myPosition);
+		logger.error("  limit    = %5d", myLimit);
+		logger.error("  capacity = %5d", myCapacity);
+		ERROR();
+	}
+	quint8* data = myData + index;
+	data[0] = (quint8)(value >> 24);
+	data[1] = (quint8)(value >> 16);
+	data[2] = (quint8)(value >>  8);
+	data[3] = (quint8)(value >>  0);
+}
+void ByteBuffer::write48(const int index, quint64 value) {
+	const int writeSize = 6;
+	if (myCapacity < (index + writeSize)) {
+		logger.error("Exceed capacity");
+		logger.error("  capacity  = %5d", myCapacity);
+		logger.error("  index     = %5d", index);
+		logger.error("  writeSize = %5d", writeSize);
+		ERROR();
+	}
+	quint8* data = myData + index;
+	data[0] = (quint8)(value >> 40);
+	data[1] = (quint8)(value >> 32);
+	data[2] = (quint8)(value >> 24);
+	data[3] = (quint8)(value >> 16);
+	data[4] = (quint8)(value >>  8);
+	data[5] = (quint8)(value >>  0);
+}
+void ByteBuffer::write(const int index, const int writeSize, const quint8* value) {
+	if (myCapacity < (index + writeSize)) {
+		logger.error("Exceed capacity");
+		logger.error("  capacity  = %5d", myCapacity);
+		logger.error("  index     = %5d", index);
+		logger.error("  writeSize = %5d", writeSize);
+		ERROR();
+	}
+	quint8* data = myData + index;
+	memcpy(data, value, writeSize);
+}
+
+
---
+++ src/util/ByteBuffer.h
@@ -0,0 +1,169 @@
+/*******************************************************************************
+ * Copyright (c) 2021, Yasuhiro Hasegawa
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+
+
+//
+// ByteBuffer.h
+//
+
+#ifndef BYTEBUFFER_H__
+#define BYTEBUFFER_H__
+
+#include "Util.h"
+
+class ByteBuffer {
+	static constexpr int INVALID_POS = -1;
+
+	int     myPosition;
+	int     myLimit;
+	int     myCapacity;
+	int     myMarkPos;
+	quint8 *myData;
+
+public:
+	ByteBuffer(int capacity, quint8* data) : myPosition(0), myLimit(capacity), myCapacity(capacity), myMarkPos(INVALID_POS), myData(data) {}
+
+	ByteBuffer(const ByteBuffer& that) {
+		this->myPosition = that.myPosition;
+		this->myLimit    = that.myLimit;
+		this->myCapacity = that.myCapacity;
+		this->myMarkPos  = that.myMarkPos;
+		this->myData     = that.myData;
+	}
+	ByteBuffer& operator =(const ByteBuffer& that) {
+		this->myPosition = that.myPosition;
+		this->myLimit    = that.myLimit;
+		this->myCapacity = that.myCapacity;
+		this->myMarkPos  = that.myMarkPos;
+		this->myData     = that.myData;
+		return *this;
+	}
+
+	// copy from data to ByteBuffer
+	void copyFrom(int len, const quint8* data);
+
+	int position() {
+		return myPosition;
+	}
+	int limit() {
+		return myLimit;
+	}
+	int capacity() {
+		return myCapacity;
+	}
+
+	int remaining() {
+		return myLimit - myPosition;
+	}
+	bool hasRemaining() {
+		return myPosition < myLimit;
+	}
+
+	// prepare for read buffer from beginning
+	void rewind() {
+		myPosition = 0;
+	}
+	// prepare for read buffer from beginning after write
+	void flip() {
+		myLimit    = myPosition;
+		myPosition = 0;
+	}
+	// prepare for write buffer from beginning
+	void clear() {
+		myPosition = 0;
+		myLimit   = 0;
+	}
+
+	// set position
+	void position(int newValue);
+	// mark current position for reset
+	void mark();
+	// set position to marked position
+	void reset();
+
+
+	// read from ByteBuffer
+	void read8 (quint8&  value) {
+		read8(myPosition, value);
+		myPosition += 1;
+	}
+	void read16(quint16& value) {
+		read16(myPosition, value);
+		myPosition += 2;
+	}
+	void read32(quint32& value) {
+		read32(myPosition, value);
+		myPosition += 4;
+	}
+	void read48(quint64& value) {
+		read48(myPosition, value);
+		myPosition += 6;
+	}
+	void read  (const int readSize, quint8* value) {
+		read(myPosition, readSize, value);
+		myPosition += readSize;
+	}
+
+	void read8 (const int index, quint8&  value);
+	void read16(const int index, quint16& value);
+	void read32(const int index, quint32& value);
+	void read48(const int index, quint64& value);
+	void read  (const int index, const int readSize, quint8* value);
+
+	// write to ByteBuffer
+	void write8 (quint8  value) {
+		write8(myPosition, value);
+		myLimit = myPosition += 1;
+	}
+	void write16(quint16 value) {
+		write16(myPosition, value);
+		myLimit = myPosition += 2;
+	}
+	void write32(quint32 value) {
+		write32(myPosition, value);
+		myLimit = myPosition += 4;
+	}
+	void write48(quint64 value) {
+		write48(myPosition, value);
+		myLimit = myPosition += 6;
+	}
+	void write  (const int writeSize, const quint8* value) {
+		write(myPosition, writeSize, value);
+		myLimit = myPosition += writeSize;
+	}
+
+	void write8 (const int index, quint8  value);
+	void write16(const int index, quint16 value);
+	void write32(const int index, quint32 value);
+	void write48(const int index, quint64 value);
+	void write  (const int index, const int writeSize, const quint8* value);
+};
+
+#endif

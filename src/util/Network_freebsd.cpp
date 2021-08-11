efcefdaa24edc078230eda08e9b36c664296595f
hasegawa <hasegawa@localhost>
2021-08-11 21:34:36 +0900

    Temporary commit

--- src/util/Network_freebsd.cpp
+++ src/util/Network_freebsd.cpp
@@ -40,47 +40,49 @@ static const Logger logger = Logger::getLogger("net-freebsd");
 #include <sys/ioctl.h>
 #include <ifaddrs.h>
 
+#include <unistd.h>
+
+#include <QtCore>
 
 // add user to group wheel to get access /dev/bpf
 // OSX
 //   dseditgroup -o edit -a hasegawa -t use wheel
 // FreeBSD
 //   pw group mod wheel -m hasegawa
+//   sudo chmod 660 /dev/bpf
 
+
+// IMPORTANT
+// To enable XNS packet sent to/from Jail, change following sysctl
+// Default setting is 1. So cannot see XNS packet from other Jail
+//   sysctl net.link.bridge.pfil_onlyip=0
 
 
 static QList<XNS::Device> getDeviceList_() {
 	QList<XNS::Device> list;
 
 	struct ifaddrs *ifap;
-	int ret = ::getifaddrs(&ifap);
-	if (ret != 0) {
-		int myErrno = errno;
-		logger.error("Unexpected getifaddrs");
-		logger.error("  error = %d  %s", myErrno, strerror(myErrno));
-		ERROR();
-	}
+	int ret;
+
+	CHECK_SYSCALL(ret, ::getifaddrs(&ifap))
 
 	for(; ifap; ifap = ifap->ifa_next) {
-		struct sockaddr *p = ifap->ifa_addr;
+		auto *p = ifap->ifa_addr;
 		if (p->sa_family == AF_LINK) {
 			struct sockaddr_dl *sdl = (struct sockaddr_dl *) ifap->ifa_addr;
 			if (sdl->sdl_alen != 0) {
+				quint8* data = (quint8*)sdl->sdl_data;
+
+				// build device
 				XNS::Device device;
 
 				// copy name
-				{
-					char* q = sdl->sdl_data;
-					for(int i = 0; i < sdl->sdl_nlen; i++) {
-						device.name.append(q[i]);
-					}
+				for(int i = 0; i < sdl->sdl_nlen; i++) {
+					device.name.append(data[i]);
 				}
 				// copy address
-				{
-					quint8* q = (quint8*)sdl->sdl_data + sdl->sdl_nlen;
-					XNS::Address address(q);
-					device.address = address;
-				}
+				XNS::Address address(data + sdl->sdl_nlen);
+				device.address = address;
 
 				list += device;
 //				logger.info("device %s", device.toString());
@@ -99,88 +101,169 @@ QList<XNS::Device> Network::getDeviceList() {
 }
 
 
-//Network::Interface::Interface(const QString& name) {
-//	logger.info("name %s", name);
-//	bool foundEthernet = false;
-//	for(auto e: Network::getEthernetList()) {
-//		if (e.name == name) {
-//			this->ethernet = e;
-//			foundEthernet = true;
-//			break;
-//		}
-//	}
-//	if (!foundEthernet) {
-//		logger.error("Unexpected name");
-//		logger.error("  name %s!", name);
-//		ERROR();
-//	}
-//
-//	{
-//		const char deviceNamePattern[] = "/dev/bpf000";
-//
-//		char deviceName[sizeof(deviceNamePattern)];
-//
-//		for(int i = 0; i < 999; i++) {
-//			sprintf(deviceName, "/dev/bpf%d", i);
-//			logger.info("open %s!", deviceName);
-//			fd = open( deviceName, O_RDONLY );
-//			if (fd == -1) {
-//				if (errno == EACCES) {
-//					logger.error("need to read and write permission to /dev/bpf");
-//					ERROR();
-//				}
-//				if (errno == ENOENT) {
-//					logger.error("device /dev/bpf does not exist");
-//					ERROR();
-//				}
-//				// continue to next device
-//				logger.warn("open error");
-//				logger.error("  name  = %s", deviceName);
-//				logger.error("  error = %d  %s", errno, strerror(errno));
-//				continue;
-//			}
-//			break;
-//		}
-//		logger.info("fd = %d", fd);
-//	}
-//
-//	// BIOCSETIF
-//	{
-//		struct ifreq ifr;
-//		memset(&ifr, 0, sizeof(ifr));
-//		::strncpy(ifr.ifr_name, TO_CSTRING(ethernet.name), IFNAMSIZ - 1);
-//	    int ret = ioctl(fd, BIOCSETIF, &ifr);
-//		logger.info("BIOCSETIF %d", ret);
-//		if (ret == -1) {
-//			logger.error("  error = %d  %s", errno, strerror(errno));
-//		}
-//	}
-//	// BIOCPROMISC
-//	{
-//		int ret = ioctl(fd, BIOCPROMISC, NULL);
-//		logger.info("BIOCPROMISC %d", ret);
-//		if (ret == -1) {
-//			logger.error("  error = %d  %s", errno, strerror(errno));
-//		}
-//	}
-//	// BIOCIMMEDIATE
-//	{
-//		int value = 1; // 1 for immediate mode
-//		int ret = ioctl(fd, BIOCIMMEDIATE, &value);
-//		logger.info("BIOCIMMEDIATE %d", ret);
-//		if (ret == -1) {
-//			logger.error("  error = %d  %s", errno, strerror(errno));
-//		}
-//	}
-//	// BIOCGHDRCMPLT
-//	{
-//		int value = 1; // 1 for no automatic setting of source address
-//		int ret = ioctl(fd, BIOCGHDRCMPLT, &value);
-//		logger.info("BIOCGHDRCMPLT %d", ret);
-//		if (ret == -1) {
-//			logger.error("  error = %d  %s", errno, strerror(errno));
-//		}
-//	}
-//}
+class FreeBSDDriver : public Network::Driver {
+public:
+	int select  (int& opError, quint32 timeout = 1); // returns return value of of select().  default timeout is 1 second
+	int transmit(int& opError, XNS::Packet& data);   // returns return value of send()
+	int receive (int& opError, XNS::Packet& data);   // returns return value of of recv()
+
+	// discard received packet
+	void discard() = 0;
+
+	FreeBSDDriver(const XNS::Device& device_);
+
+	void openBPF();
+	void prepareBPF();
+
+	int     bufferSize;
+	quint8* buffer;
+
+	std::list<XNS::Packet> list;
+	QString bpfPath;
+};
+
+int FreeBSDDriver::select  (int& opErrno, quint32 timeout) {
+	if (!list.empty()) {
+		return 1;
+	}
+
+	fd_set fds;
+	FD_ZERO(&fds);
+	FD_SET(fd, &fds);
+
+	struct timeval t;
+	t.tv_sec  = timeout;
+	t.tv_usec = 0;
+
+	int ret = ::select(FD_SETSIZE, &fds, NULL, NULL, &t);
+	opErrno = errno;
+	return ret;
+}
+int FreeBSDDriver::transmit(int& opErrno, XNS::Packet& packet) {
+	int ret = ::send(fd, packet.data, packet.limit(), 0);
+	opErrno = errno;
+	return ret;
+}
+int FreeBSDDriver::receive(int& opErrno, XNS::Packet& packet) {
+	// FIXME
+	(void)opErrno;
+	(void)packet;
+	return 0;
+}
+
+#define LOG_RET_ERRNO(ret, errNo) logger.error(#ret " = %d  errno = %d  %s", ret, errNo, strerror(errNo))
+#define CHECK_RET(ret) {if (ret < 0) { int opErrno = errno; LOG_RET_ERRNO(ret, opErrno); ERROR(); }}
+
+void FreeBSDDriver::discard() {
+	list.clear();
+
+	int readCount = 0;
+	int ret;
+	for(;;) {
+		{
+			fd_set fds;
+			FD_ZERO(&fds);
+			FD_SET(fd, &fds);
+
+			struct timeval t;
+			t.tv_sec  = 0;
+			t.tv_usec = 0;
+
+			ret = ::select(FD_SETSIZE, &fds, NULL, NULL, &t);
+			CHECK_RET(ret);
+		}
+		if (ret == 0) break;
+		if (0 < ret) {
+			ret = ::read(fd, buffer, bufferSize);
+			CHECK_RET(ret);
+		}
+	}
+	logger.debug("discards %d", readCount);
+}
 
 
+void FreeBSDDriver::openBPF() {
+	const char pathPattern[] = "/dev/bpf000";
+	char path[sizeof(pathPattern)];
+
+	for(int i = 0; i < 99; i++) {
+		sprintf(path, "/dev/bpf%d", i);
+		logger.info("open %s!", path);
+
+		fd = ::open(path, O_RDONLY);
+		if (fd < 0) {
+			int opErrno = errno;
+			LOG_ERRNO(opErrno);
+			continue;
+		}
+		break;
+	}
+	bpfPath = path;
+	logger.info("bpfPath  %s", bpfPath);
+	logger.info("fd       %d", fd);
+}
+
+void FreeBSDDriver::prepareBPF() {
+	// BIOCSETIF
+	{
+		struct ifreq ifr;
+		memset(&ifr, 0, sizeof(ifr));
+		::strncpy(ifr.ifr_name, TO_CSTRING(device.name), IFNAMSIZ - 1);
+		int ret = ::ioctl(fd, BIOCSETIF, &ifr);
+		int opErrno = errno;
+		if (ret == -1) {
+			logger.error("BIOCSETIF %d", ret);
+			logger.error("  error = %d  %s", opErrno, strerror(opErrno));
+			ERROR();
+		}
+	}
+	// BIOCPROMISC
+	{
+		int ret = ::ioctl(fd, BIOCPROMISC, NULL);
+		int opErrno = errno;
+		if (ret == -1) {
+			logger.error("BIOCPROMISC %d", ret);
+			logger.error("  error = %d  %s", opErrno, strerror(opErrno));
+			ERROR();
+		}
+	}
+	// BIOCIMMEDIATE
+	{
+		int value = 1; // 1 for immediate mode
+		int ret = ioctl(fd, BIOCIMMEDIATE, &value);
+		int opErrno = errno;
+		if (ret == -1) {
+			logger.error("BIOCIMMEDIATE %d", ret);
+			logger.error("  error = %d  %s", opErrno, strerror(opErrno));
+		}
+	}
+	// BIOCGHDRCMPLT
+	{
+		int value = 1; // 1 for no automatic setting of source address
+		int ret = ioctl(fd, BIOCGHDRCMPLT, &value);
+		int opErrno = errno;
+		if (ret == -1) {
+			logger.error("BIOCGHDRCMPLT %d", ret);
+			logger.error("  error = %d  %s", opErrno, strerror(opErrno));
+			ERROR();
+		}
+	}
+	// BIOCGBLEN
+	{
+		int value;
+		int ret = ioctl(fd, BIOCGBLEN, &value);
+		int opErrno = errno;
+		if (ret == -1) {
+			logger.error("BIOCGBLEN %d", ret);
+			logger.error("  error = %d  %s", opErrno, strerror(opErrno));
+			ERROR();
+		}
+		bufferSize = value;
+		buffer = (quint8*)::malloc(bufferSize);
+		logger.info("buffer   %d  %p", bufferSize, buffer);
+	}
+}
+FreeBSDDriver::FreeBSDDriver(const XNS::Device& device_) : Driver(device_) {
+	openBPF();
+	prepareBPF();
+}

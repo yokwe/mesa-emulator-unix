a6b2953ced30fd7685109a0cfb819e1a8b687f58
hasegawa <hasegawa@localhost>
2021-08-22 17:08:20 +0900

    Add Boot.cpp/h

---
+++ src/xns/Boot.h
@@ -0,0 +1,133 @@
+/*******************************************************************************
+ * Copyright (c) 2021, Yasuhiro Hasegawa
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+
+
+//
+// Boot.h
+//
+
+#pragma once
+
+#include <QtCore>
+
+#include "../util/Network.h"
+#include "../util/ByteBuffer.h"
+#include "../util/NameMap.h"
+
+#include "XNS.h"
+
+#include <variant>
+
+namespace XNS {
+	using ByteBuffer::UINT16;
+	using ByteBuffer::BLOCK;
+	using ByteBuffer::Buffer;
+	using ByteBuffer::Base;
+
+	//
+	// See APilot/15.0.1/Pilot/Friends/BootServerTypes.mesa
+	//
+	class Boot : public Base {
+	public:
+		class Type : public UINT16 {
+		public:
+			enum Value {
+				SIMPLE_REQUEST = 1, SIMPLE_DATA = 2, SPP_REQUEST = 3,
+			};
+
+			// define operator =
+			quint16 operator =(const quint16& newValue) const {
+				value(newValue);
+				return newValue;
+			}
+
+			QString toString() const {
+				return nameMap.toString(value());
+			}
+			static void addNameMap(quint16 value, QString name) {
+				nameMap.add(value, name);
+			}
+		private:
+			static NameMap::Map<quint16> nameMap;
+		};
+
+		class SimpleRequest : public Base {
+		public:
+			IDP::Host bootFileNumber; // XNS::IDG::HOST::BFN_GVWIN
+
+			QString toString() const;
+
+			// ByteBuffer::Base
+			void fromByteBuffer(Buffer& bb);
+			void toByteBuffer  (Buffer& bb) const;
+		};
+		class SimpleData : public Base {
+		public:
+			IDP::Host bootFileNumber; // XNS::IDG::HOST::BFN_GVWIN
+			UINT16    packetNumber;   // start with 1
+			BLOCK     block;          // one page of data normally or no data means end of file
+
+			QString toString() const;
+
+			// ByteBuffer::Base
+			void fromByteBuffer(Buffer& bb);
+			void toByteBuffer  (Buffer& bb) const;
+		};
+		class SPPRequest : public Base {
+		public:
+			IDP::Host bootFileNumber; // XNS::IDG::HOST::BFN_GVWIN
+			UINT16    connectionID;
+
+			QString toString() const;
+
+			// ByteBuffer::Base
+			void fromByteBuffer(Buffer& bb);
+			void toByteBuffer  (Buffer& bb) const;
+		};
+
+		Type type;
+		std::variant<std::monostate, class SimpleRequest, class SimpleData, class SPPRequest> body;
+
+		void get(SimpleRequest& newValue) const;
+		void get(SimpleData&    newValue) const;
+		void get(SPPRequest&    newValue) const;
+
+		void set(const SimpleRequest& newValue);
+		void set(const SimpleData&    newValue);
+		void set(const SPPRequest&    newValue);
+
+		QString toString() const;
+
+		// ByteBuffer::Base
+		void fromByteBuffer(Buffer& bb);
+		void toByteBuffer  (Buffer& bb) const;
+	};
+
+}
--- src/xns/XNS.pro
+++ src/xns/XNS.pro
@@ -7,8 +7,8 @@ CONFIG  += staticlib
 HEADERS += XNS.h   Config.h   Courier.h
 SOURCES += XNS.cpp Config.cpp Courier.cpp
 
-HEADERS += RIP.h   Error.h   Echo.h   SPP.h   PEX.h
-SOURCES += RIP.cpp Error.cpp Echo.cpp SPP.cpp PEX.cpp
+HEADERS += RIP.h   Error.h   Echo.h   SPP.h   PEX.h   Boot.h
+SOURCES += RIP.cpp Error.cpp Echo.cpp SPP.cpp PEX.cpp Boot.cpp
 
 HEADERS += Time.h
 SOURCES += Time.cpp

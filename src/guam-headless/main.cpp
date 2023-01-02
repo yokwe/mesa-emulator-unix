7ca34212babb706bd4b73289e95e4ed76385a989
hasegawa <hasegawa@macmini2020.lan>
2023-01-02 22:52:47 +0900

    Take one argument

--- src/guam-headless/main.cpp
+++ src/guam-headless/main.cpp
@@ -55,16 +55,14 @@ int main(int argc, char** argv) {
 	GuiOp::setContext(new NullGuiOp);
 
 	// sanity check
-	if (argc != 3) {
+	if (argc != 2) {
 		logger.error("Unexpected argc %d", argc);
 		ERROR();
 	}
-	QString basePath  = argv[1];
-	QString entryName = argv[2];
-	logger.info("basePath  = %s", qPrintable(basePath));
+	QString entryName = argv[1];
 	logger.info("entryName = %s", qPrintable(entryName));
 
-	Setting setting = Setting::getInstance(basePath);
+	Setting setting = Setting::getInstance();
 	Setting::Entry entry = setting.getEntry(entryName);
 
 	CARD32  displayWidth     = entry.display.width;

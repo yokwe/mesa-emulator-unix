2c331629472f808259f7c43df3a3418cc9ef7d0a
hasegawa <hasegawa@macmini2020.lan>
2023-01-13 09:57:22 +0900

    Move glob_to_regex to json namespace

--- src/json/json.cpp
+++ src/json/json.cpp
@@ -26,7 +26,7 @@ public:
 
 	void addPathFilter(std::string glob) {
 		logger.info("pathFilter %s", glob);
-		std::string regex = json::impl::glob_to_regex(glob);
+		std::string regex = json::glob_to_regex(glob);
 		std::regex re(regex);
 		pathFilters.push_back(re);
 	}
@@ -39,7 +39,7 @@ public:
 	void skipUntil(const std::string& path, const std::string& value) {
 		logger.info("skipUntil %s %s", path, value);
 		skipFlag  = true;
-		std::string regex = json::impl::glob_to_regex(path);
+		std::string regex = json::glob_to_regex(path);
 		std::regex re(regex);
 		skipPath  = re;
 		skipValue = value;
@@ -47,7 +47,8 @@ public:
 	//
 	// value
 	//
-	bool process(const std::string& path, const json::impl::json_value& json_value) {
+	bool process(const std::string& path, const std::string& name, const json::impl::json_value& json_value) {
+		(void)name; // avoid unused parameter
 		const std::string& value = json_value.to_string();
 
 		if (skipFlag && value == skipValue && std::regex_match(path, skipPath)) {
@@ -66,14 +67,61 @@ public:
 	//
 	// container
 	//
-	void process(json::impl::json_container& container) {
+	void process(const std::string& path, const std::string& name, json::impl::json_container& json_container) {
+		(void)name; // avoid unused parameter
 		if (skipFlag) return;
 
-		const std::string& path = container.getPath();
-		if (matchPathFilter(path)) container.setFilter();
+		if (matchPathFilter(path)) json_container.setFilter();
 	}
 };
 
+}
+
+
+//
+// utility function
+//
+std::string json::glob_to_regex(std::string glob) {
+	std::string ret;
+	for(size_t i = 0; i < glob.size(); i++) {
+		char c = glob[i];
+		switch(c) {
+		case '?':
+			ret += "[^/]";
+			break;
+		case '*':
+			if ((i + 1) == glob.size()) {
+				ret += "[^/]*?"; // *
+			} else {
+				char next = glob[i + 1];
+				if (next == '*') {
+					ret += ".*?"; // **
+					i++;
+				} else {
+					ret += "[^/]*?"; // *
+				}
+			}
+			break;
+		case '+':
+		case '^':
+		case '$':
+		case '(':
+		case ')':
+		case '[':
+		case ']':
+		case '{':
+		case '}':
+		case '|':
+		case '\\':
+			ret += '\\';
+			ret += c;
+			break;
+		default:
+			ret += c;
+			break;
+		}
+	}
+	return ret;
 }
 
 
--- src/json/json.h
+++ src/json/json.h
@@ -8,8 +8,11 @@
 
 namespace json {
 //
+// utility function
 //
-//
+std::string glob_to_regex(std::string glob);
+
+
 int dump(std::istream& in);
 
 }

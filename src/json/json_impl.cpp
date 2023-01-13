1730f04a67709b233dbed5b8c7c9e1c0b0a5c44a
hasegawa <hasegawa@macmini2020.lan>
2023-01-13 10:03:28 +0900

    Move glog_to_regex in json namespace

--- src/json/json_impl.cpp
+++ src/json/json_impl.cpp
@@ -12,53 +12,6 @@ namespace impl {
 
 
 //
-// utility function
-//
-std::string glob_to_regex(std::string glob) {
-	std::string ret;
-	for(size_t i = 0; i < glob.size(); i++) {
-		char c = glob[i];
-		switch(c) {
-		case '?':
-			ret += "[^/]";
-			break;
-		case '*':
-			if ((i + 1) == glob.size()) {
-				ret += "[^/]*?"; // *
-			} else {
-				char next = glob[i + 1];
-				if (next == '*') {
-					ret += ".*?"; // **
-					i++;
-				} else {
-					ret += "[^/]*?"; // *
-				}
-			}
-			break;
-		case '+':
-		case '^':
-		case '$':
-		case '(':
-		case ')':
-		case '[':
-		case ']':
-		case '{':
-		case '}':
-		case '|':
-		case '\\':
-			ret += '\\';
-			ret += c;
-			break;
-		default:
-			ret += c;
-			break;
-		}
-	}
-	return ret;
-}
-
-
-//
 // json_value
 //
 const std::string json_value::NULL_STRING  = "NULL";

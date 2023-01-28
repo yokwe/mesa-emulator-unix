98288e371da3693e9705fe4c15c4a491036f8abb
hasegawa <hasegawa@macmini2020.lan>
2023-01-28 23:16:04 +0900

    Temporary commit.

--- src/json/stream.h
+++ src/json/stream.h
@@ -9,9 +9,9 @@
 namespace stream {
 
 template <typename T>
-class java_iterator_t {
+class iterator_t {
 public:
-	virtual ~java_iterator_t() {}
+	virtual ~iterator_t() {}
 
 	virtual bool has_next() = 0;
 	virtual T    next()     = 0;
@@ -21,14 +21,14 @@ enum type_t {
 	HEAD, BODY, TAIL,
 };
 
-class base_t;
+
 class base_t {
 public:
-	base_t(type_t type, const char* name, base_t* previous) :
-		m_type(type), m_name(name), m_previous(previous), m_closed(false) {}
+	base_t(type_t type, const char* name) :
+		m_type(type), m_name(name), m_closed(false) {}
 	virtual ~base_t() {}
 
-	virtual void close_impl() = 0;
+	void close_impl() {} // do nothing
 
 	void close() {
 		if (!closed()) {
@@ -50,20 +50,14 @@ public:
 protected:
 	type_t         m_type;
 	const char*    m_name;
-	base_t*        m_previous;
 	bool           m_closed;
 };
 
 
 template <typename T>
-class head_t : public base_t, public java_iterator_t<T> {
+class head_t : public base_t, public iterator_t<T> {
 public:
-	head_t(const char* name) : base_t(type_t::HEAD, name, nullptr) {
-		assert(m_previous == nullptr);
-	}
-	head_t(type_t type, const char* name, base_t* previous) : base_t(type, name, previous) {
-		assert(m_previous != nullptr);
-	}
+	head_t(const char* name) : base_t(type_t::HEAD, name) {}
 	virtual ~head_t() {}
 
 	// need to implement virtual method of parent classes
@@ -73,17 +67,17 @@ public:
 	virtual T    next_impl()     = 0;
 
 	bool has_next() override {
-		if (m_closed) {
+		if (base_t::closed()) {
 			return false;
 		} else {
 			bool ret = has_next_impl();
 			// if no more data, call close
-			if (!ret) close();
+			if (!ret) base_t::close();
 			return ret;
 		}
 	}
 	T next() override {
-		if (m_closed) {
+		if (base_t::closed()) {
 			logger.error("logger is already closed");
 			ERROR();
 		} else {
@@ -95,12 +89,11 @@ public:
 
 template <typename T, typename R>
 class tail_t : public base_t {
-	head_t<T>* m_head;
+	iterator_t<T>* m_iterator;
 public:
-	tail_t(const char* name, base_t* previous) : base_t(type_t::TAIL, name, previous) {
-		m_head = dynamic_cast<head_t<T>*>(previous);
-		assert(m_head != nullptr);
-	}
+	tail_t(const char* name, base_t* previous) :
+		base_t(type_t::TAIL, name),
+		m_iterator(dynamic_cast<iterator_t<T>*>(previous)) {}
 	virtual ~tail_t() {}
 
 	// need to implement virtual method of parent classes
@@ -110,7 +103,7 @@ public:
 	virtual R    result_impl()            = 0;
 
 	R result() {
-		if (closed()) {
+		if (base_t::closed()) {
 			return result_impl();
 		} else {
 			ERROR();
@@ -118,7 +111,7 @@ public:
 	}
 
 	void accept(T& newValue) {
-		if (closed()) {
+		if (base_t::closed()) {
 			ERROR();
 		} else {
 			accept_impl(newValue);
@@ -126,12 +119,34 @@ public:
 	}
 
 	R process() {
-		while(m_head->has_next()) {
-			auto t = m_head->next();
+		while(m_iterator->has_next()) {
+			T t = m_iterator->next();
 			accept(t);
 		}
-		close();
+		base_t::close();
 		return result();
+	}
+};
+
+
+// 1 to 1
+template <typename T, typename R>
+class body11_t : public base_t, public iterator_t<R> {
+	iterator_t<T>* m_iterator;
+public:
+	body11_t(const char* name, base_t* previous) :
+		base_t(type_t::BODY, name),
+		m_iterator(dynamic_cast<iterator_t<T>*>(previous)) {}
+	virtual ~body11_t() {}
+
+	virtual R apply(T& newValue) = 0;
+
+	bool has_next() {
+		return m_iterator->has_next();
+	}
+	R next() {
+		T newValue = m_iterator->next();
+		return apply(newValue);
 	}
 };
 

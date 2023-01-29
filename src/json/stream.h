e980f1a89e736c5e9e55575a015ecc7f2c9cd957
hasegawa <hasegawa@macmini2020.lan>
2023-01-29 23:15:18 +0900

    Reorganize structure.

--- src/json/stream.h
+++ src/json/stream.h
@@ -11,20 +11,9 @@
 namespace stream {
 
 
-template <typename T>
-class iterator_t {
-public:
-	virtual ~iterator_t() {}
-
-	virtual bool has_next() = 0;
-	virtual T    next()     = 0;
-};
-
-
 class base_t {
 public:
-	base_t(const char* name) : m_name(name), m_closed(false), m_upstream(nullptr) {}
-	base_t(const char* name, base_t* upstream) : m_name(name), m_closed(false), m_upstream(upstream) {}
+	base_t(const char* name) : m_name(name), m_closed(false) {}
 	virtual ~base_t() {}
 
 	virtual void close_impl() = 0; // most of case do nothing
@@ -39,32 +28,31 @@ public:
 	const char* name() {
 		return m_name;
 	}
-	base_t* upstream() {
-		return m_upstream;
-	}
 
 private:
-	const char*    m_name;
-	bool           m_closed;
-	base_t*        m_upstream;
+	const char* m_name;
+	bool        m_closed;
 };
 
 
 template <typename T>
-class head_t : public base_t, public iterator_t<T> {
+class source_t : public base_t {
 	// You need to implement base_t::close_impl() in implementing class
 	// void close_impl();
 	virtual bool has_next_impl() = 0;
 	virtual T    next_impl()     = 0;
 
 public:
-	head_t(const char* name) : base_t(name) {}
-	virtual ~head_t() {}
+	const ::std::string data_type_name = demangle(typeid(T).name());
 
-	bool has_next() override {
+	source_t(const char* name) : base_t(name) {}
+
+	virtual ~source_t() {}
+
+	bool has_next() {
 		return has_next_impl();
 	}
-	T next() override {
+	T next() {
 		if (has_next()) {
 			return next_impl();
 		} else {
@@ -77,33 +65,33 @@ public:
 
 
 template <typename T>
-class vector_t : public head_t<T> {
+class vector_t : public source_t<T> {
 	::std::vector<T> m_data;
 	::std::size_t    m_pos;
 public:
 	vector_t(::std::vector<T>& data) :
-		head_t<T>(__func__), m_data(data),
+		source_t<T>(__func__), m_data(data),
 		m_pos(0) {}
 	vector_t(::std::initializer_list<T> init) :
-		head_t<T>(__func__), m_data(init.begin(), init.end()),
+		source_t<T>(__func__), m_data(init.begin(), init.end()),
 		m_pos(0) {}
 	~vector_t() {
 		base_t::close();
 	}
 private:
-	void close_impl() {}
-	bool has_next_impl() {
+	void close_impl() override {}
+	bool has_next_impl() override {
 		return 0 <= m_pos && m_pos < m_data.size();
 	}
-	T next_impl() {
+	T next_impl() override {
 		return m_data[m_pos++];
 	}
 };
 
 
 template <typename T, typename R>
-class tail_t : public base_t {
-	iterator_t<T>* upstream;
+class sink_t : public base_t {
+	source_t<T>* upstream;
 
 	// You need to implement base_t::close_impl() in implementing class
 	// void close_impl();
@@ -111,19 +99,11 @@ class tail_t : public base_t {
 	virtual R    result_impl()            = 0;
 
 public:
-	tail_t(const char* name, base_t* upstream_) :
-		base_t(name, upstream_),
-		upstream(dynamic_cast<iterator_t<T>*>(upstream_)) {
-		if (upstream == nullptr) {
-			logger.error("upstream doesn't have iterator");
-			logger.error("  myself    %s!", this->name());
-			logger.error("  upstream_ %s!", upstream_->name());
-			logger.error("  T         %s!", demangle(typeid(T).name()));
-			logger.error("  R         %s!", demangle(typeid(R).name()));
-			ERROR();
-		}
+	sink_t(const char* name, source_t<T>* upstream_) :
+		base_t(name),
+		upstream(upstream_) {
 	}
-	virtual ~tail_t() {}
+	virtual ~sink_t() {}
 
 	R result() {
 		return result_impl();
@@ -156,7 +136,7 @@ public:
 
 
 template <typename T, typename R=T>
-class sum_t : public tail_t<T, R> {
+class sum_t : public sink_t<T, R> {
 	static_assert(::std::is_integral<T>::value || ::std::is_floating_point<T>::value,  "T is not number");
 	static_assert(::std::is_integral<R>::value || ::std::is_floating_point<R>::value,  "R is not number");
 
@@ -170,7 +150,7 @@ class sum_t : public tail_t<T, R> {
 		return sum;
 	}
 public:
-	sum_t(base_t* upstream) : tail_t<T, R>(__func__, upstream), sum(0) {}
+	sum_t(source_t<T>* upstream) : sink_t<T, R>(__func__, upstream), sum(0) {}
 	~sum_t() {
 		base_t::close();
 	}
@@ -178,7 +158,7 @@ public:
 
 
 template <typename T, typename R=int>
-class count_t : public tail_t<T, R> {
+class count_t : public sink_t<T, R> {
 	static_assert(::std::is_integral<R>::value || ::std::is_floating_point<R>::value,  "R is not number");
 
 	R m_count;
@@ -192,7 +172,7 @@ class count_t : public tail_t<T, R> {
 		return m_count;
 	}
 public:
-	count_t(base_t* upstream) : tail_t<T, R>(__func__, upstream), m_count(0) {}
+	count_t(base_t* upstream) : sink_t<T, R>(__func__, upstream), m_count(0) {}
 	~count_t() {
 		base_t::close();
 	}
@@ -200,79 +180,24 @@ public:
 
 
 template <typename T, typename R=T>
-class map_abstract_t : public base_t, public iterator_t<R> {
-	iterator_t<T>* upstream;
-
-	// You need to implement base_t::close_impl() in implementing class
-	// void close_impl();
-	virtual R apply(T& newValue) = 0;
-
-public:
-	map_abstract_t(const char* name, base_t* upstream_) :
-		base_t(name, upstream_),
-		upstream(dynamic_cast<iterator_t<T>*>(upstream_)) {
-		if (upstream == nullptr) {
-			logger.error("upstream doesn't have iterator");
-			logger.error("  myself    %s!", this->name());
-			logger.error("  upstream_ %s!", upstream_->name());
-			logger.error("  T         %s!", demangle(typeid(T).name()));
-			logger.error("  R         %s!", demangle(typeid(R).name()));
-			ERROR();
-		}
-	}
-	virtual ~map_abstract_t() {}
-
-	bool has_next() override {
-		return upstream->has_next();
-	}
-	R next() override {
-		if (has_next()) {
-			T newValue = upstream->next();
-			return apply(newValue);
-		} else {
-			// if there is no next and call next(), it is error
-			logger.error("stream has no next");
-			ERROR();
-		}
-	}
-};
-
-
-template <typename T, typename R=T>
-class map_t : public base_t, public iterator_t<R> {
+class map_t : public source_t<R> {
 	typedef ::std::function<R(T)> func_apply;
-	iterator_t<T>* upstream;
-	func_apply     apply;
+	source_t<T>* upstream;
+	func_apply   apply;
 
 	void close_impl() override {}
-
-public:
-	map_t(base_t* upstream_, func_apply apply_) :
-		base_t(__func__, upstream_),
-		upstream(dynamic_cast<iterator_t<T>*>(upstream_)),
-		apply(apply_) {
-		if (upstream == nullptr) {
-			logger.error("upstream doesn't have iterator");
-			logger.error("  myself    %s!", this->name());
-			logger.error("  upstream_ %s!", upstream_->name());
-			logger.error("  T         %s!", demangle(typeid(T).name()));
-			logger.error("  R         %s!", demangle(typeid(R).name()));
-			ERROR();
-		}
-	}
-	bool has_next() override {
+	bool has_next_impl() override {
 		return upstream->has_next();
 	}
-	R next() override {
-		if (has_next()) {
-			T newValue = upstream->next();
-			return apply(newValue);
-		} else {
-			// if there is no next and call next(), it is error
-			logger.error("stream has no next");
-			ERROR();
-		}
+	R next_impl() override {
+		T newValue = upstream->next();
+		return apply(newValue);
 	}
+public:
+	map_t(source_t<T>* upstream_, func_apply apply_) :
+		source_t<T>(__func__),
+		upstream(upstream_),
+		apply(apply_) {}
 };
 
 
@@ -287,7 +212,7 @@ sum_t<T, R> sum(map_t<S, T>* upstream) {
 }
 
 template <typename T, typename R>
-map_t<T, R> map(base_t* upstream, ::std::function<R(T)> func) {
+map_t<T, R> map(source_t<T>* upstream, ::std::function<R(T)> func) {
 	return map_t<T, R>(upstream, func);
 }
 

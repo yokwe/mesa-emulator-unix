bc4036b15efd65dd6268a4afaf567d0acdc023c5
hasegawa <hasegawa@macmini2020.lan>
2023-02-15 19:25:10 +0900

    Move sum/count/map to json_util.h

--- src/json/stream.h
+++ src/json/stream.h
@@ -1,108 +1,101 @@
 //
-// stream.h
+// stream3.h
 //
 
 #pragma once
 
-#include <initializer_list>
-#include <tuple>
+#include <string>
 
 #include "../util/Util.h"
 
 namespace stream {
-
-
-class base_t {
-	const char* m_name;
-	bool        m_closed;
-
-public:
-	base_t(const char* name) : m_name(name), m_closed(false) {}
-	virtual ~base_t() {}
-
-	virtual void close_impl() = 0; // most of case do nothing
-	void close() {
-		if (!m_closed) {
-			m_closed = true;
-			close_impl();
-		}
-	}
-	const char* name() const {
-		return m_name;
-	}
-};
+// begin of namespace stream
 
 
 template <typename T>
-class source_t : public base_t {
-	// You need to implement base_t::close_impl() in implementing class
-	// void close_impl();
-	virtual bool has_next_impl() = 0;
-	virtual T    next_impl()     = 0;
+struct source_base_t {
+	virtual ~source_base_t() {}
 
+	virtual void close()    = 0;
+	virtual bool has_next() = 0;
+	virtual T    next()     = 0;
+};
+
+
+//
+// sink_t
+//
+template <typename T, typename R>
+class sink_t {
 public:
-	const std::string type_T = demangle(typeid(T).name());
+	using upstream_t = source_base_t<T>;
 
-	source_t(const char* name) : base_t(name) {}
-	virtual ~source_t() {}
+	struct base_t {
+		virtual ~base_t() {}
 
-	bool has_next() {
-		return has_next_impl();
+		virtual void close()      = 0;
+		virtual void accept(T& t) = 0;
+		virtual R    result()     = 0;
+	};
+private:
+	struct impl_t : public base_t {
+		template<typename U>
+		struct wrapper_t : public base_t {
+			wrapper_t(const U& u) : m_object(u) {}
+
+			void close() override {
+				m_object.close();
+			}
+			void accept(T& t) override {
+				m_object.accept(t);
+			}
+			R    result() override {
+				return m_object.result();
+			}
+
+			U m_object;
+		};
+
+		template <typename U>
+		impl_t(const U& u) : m_base(std::make_shared<wrapper_t<U>>(u)) {
+			logger.info("impl_t constructor %s", demangle(typeid(U).name()));
+		}
+
+		void close() override {
+			m_base->close();
+		}
+		void accept(T& t) override {
+			m_base->accept(t);
+		}
+		R result() override {
+			return m_base->result();
+		}
+
+		std::shared_ptr<base_t> m_base;
+	};
+
+	impl_t       m_impl;
+	std::string  m_name;
+	upstream_t*  m_upstream;
+	bool         m_closed = false;
+public:
+	sink_t(impl_t impl, const char* name, source_base_t<T>* upstream_) : m_impl(impl),  m_name(name), m_upstream(upstream_) {}
+	virtual ~sink_t() {
+		close();
 	}
-	T next() {
-		if (has_next()) {
-			return next_impl();
-		} else {
-			// if there is no next and call next(), it is error
-			logger.error("stream has no next");
-			ERROR();
+
+	void close() {
+		if (!m_closed) {
+			m_closed = true;
+			m_impl.close();
 		}
 	}
-};
-
-
-template <typename T, typename R>
-class pipe_t : public source_t<R> {
-protected:
-	source_t<T>* m_upstream;
-
-public:
-	const std::string type_T = demangle(typeid(T).name());
-	const std::string type_R = demangle(typeid(R).name());
-
-	pipe_t(const char* name, source_t<T>* upstream) : source_t<R>(name), m_upstream(upstream) {}
-	virtual ~pipe_t() {}
-};
-
-
-template <typename T, typename R>
-class sink_t : public base_t {
-	source_t<T>* m_upstream;
-
-	// You need to implement base_t::close_impl() in implementing class
-	// void close_impl();
-	virtual void accept_impl(T& newValue) = 0;
-	virtual R    result_impl()            = 0;
-
-public:
-	const std::string type_T = demangle(typeid(T).name());
-	const std::string type_R = demangle(typeid(R).name());
-
-	sink_t(const char* name, source_t<T>* upstream_) : base_t(name), m_upstream(upstream_) {}
-	virtual ~sink_t() {}
-
 	R result() {
-		return result_impl();
+		return m_impl.result();
 	}
 
 	void accept(T& newValue) {
-		accept_impl(newValue);
-	}
-	void accept(int newValue) {
-		accept_impl(newValue);
-	}
-	void accept(double newValue) {
-		accept_impl(newValue);
+		m_impl.accept(newValue);
 	}
 
 	R process() {
@@ -119,375 +112,153 @@ public:
 // source_t
 //
 template <typename T>
-class vector_t : public source_t<T> {
-	std::vector<T> m_data;
-	std::size_t    m_pos;
+class source_t : public source_base_t<T> {
+private:
+	using base_t = source_base_t<T>;
+	struct impl_t : public base_t {
+		template<typename U>
+		struct wrapper_t : public base_t {
+			wrapper_t(const U& u) : m_object(u) {}
 
-	void close_impl() override {}
-	bool has_next_impl() override {
-		return 0 <= m_pos && m_pos < m_data.size();
-	}
-	T next_impl() override {
-		return m_data[m_pos++];
-	}
+			void close() override {
+				m_object.close();
+			}
+			bool has_next() override {
+				return m_object.has_next();
+			}
+			T    next() override {
+				return m_object.next();
+			}
 
+			U m_object;
+		};
+
+		template <typename U>
+		impl_t(const U& u) : m_base(std::make_shared<wrapper_t<U>>(u)) {
+			logger.info("impl_t constructor %s", demangle(typeid(U).name()));
+		}
+
+		void close() override {
+			m_base->close();
+		}
+		bool has_next() override {
+			return m_base->has_next();
+		}
+		T next() override {
+			return m_base->next();
+		}
+
+		std::shared_ptr<base_t> m_base;
+	};
+
+	impl_t      m_impl;
+	std::string m_name;
+	bool        m_closed = false;
 public:
-	vector_t(std::vector<T>& data) :
-		source_t<T>(__func__), m_data(data),
-		m_pos(0) {}
-	vector_t(std::initializer_list<T> init) :
-		source_t<T>(__func__), m_data(init.begin(), init.end()),
-		m_pos(0) {}
-	~vector_t() {
-		base_t::close();
+	source_t(impl_t impl, const char* name) : m_impl(impl),  m_name(name) {}
+	~source_t() {
+		close();
+	}
+
+	void close() override {
+		if (!m_closed) {
+			m_closed = true;
+			m_impl.close();
+		}
+	}
+	bool has_next() override {
+		if (m_closed) {
+			return false;
+		} else {
+			return m_impl.has_next();
+		}
+	}
+	T next() override {
+		if (has_next()) {
+			return m_impl.next();
+		} else {
+			// if there is no next and call next(), it is error
+			assert(false);
+		}
 	}
 };
-template <typename T>
-vector_t<T> vector(std::initializer_list<T> init) {
-	return vector_t<T>(init);
-}
-
-
-//
-// sink_t
-//
-template <typename T, typename R=T>
-class sum_t : public sink_t<T, R> {
-	static_assert(std::is_integral_v<T> || std::is_floating_point_v<T>,  "T is not number");
-	static_assert(std::is_integral_v<R> || std::is_floating_point_v<R>,  "R is not number");
-
-	R m_sum;
-
-	void close_impl() override {}
-	void accept_impl(T& newValue) override {
-		m_sum += newValue;
-	}
-	R result_impl() override {
-		return m_sum;
-	}
-
-public:
-	sum_t(source_t<T>* upstream) : sink_t<T, R>(__func__, upstream), m_sum(0) {}
-	~sum_t() {
-		base_t::close();
-	}
-};
-template<typename T, typename R=T>
-sum_t<T, R> sum(source_t<T>* upstream) {
-	return sum_t<T, R>(upstream);
-}
-
-
-template <typename T, typename R=int>
-class count_sink_t : public sink_t<T, R> {
-	R m_count;
-	void close_impl() override {}
-	void accept_impl(T& newValue) override {
-		(void)newValue;
-		m_count++;
-	}
-	R result_impl() override {
-		return m_count;
-	}
-
-public:
-	count_sink_t(source_t<T>* upstream) : sink_t<T, R>(__func__, upstream), m_count(0) {}
-	~count_sink_t() {
-		base_t::close();
-	}
-};
-template<typename T, typename R=int>
-count_sink_t<T, R> count(source_t<T>* upstream) {
-	return count_sink_t<T, R>(upstream);
-}
 
 
 //
 // pipe_t
 //
-template <typename T, typename R=T>
-class peek_t : public pipe_t<T, R> {
-	using peek_apply = std::function<void(T)>;
-	peek_apply   m_apply;
-
-	void close_impl() override {}
-	bool has_next_impl() override {
-		return pipe_t<T, R>::m_upstream->has_next();
-	}
-	R next_impl() override {
-		R newValue = pipe_t<T, R>::m_upstream->next();
-		m_apply(newValue);
-		return newValue;
-	}
-
-public:
-	peek_t(source_t<T>* upstream, peek_apply apply) :
-		pipe_t<T, R>(__func__, upstream),
-		m_apply(apply) {}
-	~peek_t() {
-		base_t::close();
-	}
-};
-template <typename T, typename Function>
-auto peek(source_t<T>* upstream,  Function apply) {
-	using trait = trait_function<Function>;
-	using R   = typename trait::ret_type;
-	using A0_ = typename std::tuple_element<0, typename trait::arg_type>;
-	using A0  = typename A0_::type;
-
-	//logger.debug("map T=%s R=%s Function=%s", demangle(typeid(T).name()), demangle(typeid(R).name()), demangle(typeid(Function).name()));
-	static_assert(trait::arity == 1);
-	static_assert(std::is_same_v<T, A0>);
-
-	// assert T     == A0T
-	// assert arity == 1
-	if constexpr (!std::is_same_v<T, A0> || !(trait::arity == 1) || !std::is_same_v<R, void>) {
-		logger.error("ASSERTION FAILED");
-		logger.error("function %s", __func__);
-		logger.error("arity %d", trait::arity);
-		logger.error("R     %s", demangle(typeid(R).name()));
-		logger.error("T     %s", demangle(typeid(T).name()));
-		logger.error("A0    %s", demangle(typeid(A0).name()));
-		ERROR();
-	}
-
-	return peek_t<T>(upstream, apply);
-}
-
-
-template <typename Type, typename = void>
-struct has_start : std::false_type {};
-template <typename T>
-struct has_start<T, typename std::enable_if<std::is_member_function_pointer<decltype(&T::start)>::value>::type> : std::true_type {};
-//
-template <typename Type, typename = void>
-struct has_stop : std::false_type {};
-template <typename T>
-struct has_stop<T, typename std::enable_if<std::is_member_function_pointer<decltype(&T::stop)>::value>::type> : std::true_type {};
-//
-template <typename Type, typename = void>
-struct has_data : std::false_type {};
-template <typename T>
-struct has_data<T, typename std::enable_if<std::is_member_function_pointer<decltype(&T::data)>::value>::type> : std::true_type {};
-//
-template <typename T, typename R, typename Callback>
-class tee_t : public pipe_t<T, R> {
-public:
-	Callback m_callback;
-
-	tee_t(source_t<T>* upstream, Callback callback_) :
-		pipe_t<T, R>(__func__, upstream), m_callback(callback_) {
-		// sanity check
-		if constexpr (has_start<Callback>::value) {
-			if constexpr (std::is_invocable_r_v<void, decltype(&Callback::start), Callback&>) {
-				logger.info("Callback::start is void start()");
-			} else {
-				logger.info("Callback::start is not void start()");
-			}
-			static_assert(std::is_invocable_v<decltype(&Callback::start), Callback&>);
-		} else {
-			logger.info("no Callback::start");
-		}
-		if constexpr (has_stop<Callback>::value) {
-			if constexpr (std::is_invocable_r_v<void, decltype(&Callback::stop), Callback&>) {
-				logger.info("Callback::stop is void stop()");
-			} else {
-				logger.info("Callback::stop is not void stop()");
-			}
-			static_assert(std::is_invocable_v<decltype(&Callback::stop), Callback&>);
-		} else {
-			logger.info("no Callback::stop");
-		}
-		if constexpr (has_data<Callback>::value) {
-			if constexpr (std::is_invocable_r_v<void, decltype(&Callback::data), Callback&, T>) {
-				logger.info("Callback::data is void data(T)");
-			} else {
-				logger.info("Callback::data is not void data(T)");
-			}
-			static_assert(std::is_invocable_v<decltype(&Callback::data), Callback&, T>);
-		} else {
-			logger.info("no Callback::data");
-		}
-		static_assert(has_data<Callback>::value);
-
-		// call start
-		if constexpr (has_start<Callback>::value) {
-			if constexpr (std::is_invocable_v<decltype(&Callback::start), Callback&>) {
-				m_callback.start();
-			}
-		}
-	}
-	~tee_t() {
-		base_t::close();
-	}
-
-	void close_impl() override {
-		// call stop
-		if constexpr (has_stop<Callback>::value) {
-			if constexpr (std::is_invocable_v<decltype(&Callback::stop), Callback&>) {
-				m_callback.stop();
-			}
-		}
-	}
-	bool has_next_impl() override {
-		return pipe_t<T, R>::m_upstream->has_next();
-	}
-	R next_impl() override {
-		R newValue = pipe_t<T, R>::m_upstream->next();
-		m_callback.data(newValue);
-		return newValue;
-	}
-};
-template <typename T, typename Callback>
-auto tee(source_t<T>* upstream, Callback callback) {
-	return tee_t<T, T, Callback>(upstream, callback);
-}
-
-
 template <typename T, typename R>
-class map_t : public pipe_t<T, R> {
-	using map_apply = std::function<R(T)>;
+class pipe_t : public source_base_t<R> {
+private:
+	using base_t = source_base_t<R>;
+	struct impl_t : public base_t {
+		template<typename U>
+		struct wrapper_t : public base_t {
+			wrapper_t(const U& u) : m_object(u) {}
 
-	map_apply    m_apply;
+			void close() override {
+				m_object.close();
+			}
+			bool has_next() override {
+				return m_object.has_next();
+			}
+			R    next() override {
+				return m_object.next();
+			}
 
-	void close_impl() override {}
-	bool has_next_impl() override {
-		return pipe_t<T, R>::m_upstream->has_next();
-	}
-	R next_impl() override {
-		T newValue = pipe_t<T, R>::m_upstream->next();
-		return m_apply(newValue);
-	}
+			U m_object;
+		};
 
-public:
-	map_t(source_t<T>* upstream, map_apply apply) :
-		pipe_t<T, R>(__func__, upstream),
-		m_apply(apply) {}
-	~map_t() {
-		base_t::close();
-	}
-};
-template <typename T, typename Function>
-auto map(source_t<T>* upstream,  Function apply) {
-	using trait = trait_function<Function>;
-	using R   = typename trait::ret_type;
-	using A0_ = typename std::tuple_element<0, typename trait::arg_type>;
-	using A0  = typename A0_::type;
-
-	static_assert(trait::arity == 1);
-	static_assert(std::is_same_v<T, A0>);
-
-	// assert T     == A0T
-	// assert arity == 1
-	if constexpr (!std::is_same_v<T, A0> || !(trait::arity == 1)) {
-		logger.error("ASSERTION FAILED");
-		logger.error("function %s", __func__);
-		logger.error("arity %d", trait::arity);
-		logger.error("R     %s", demangle(typeid(R).name()));
-		logger.error("T     %s", demangle(typeid(T).name()));
-		logger.error("A0    %s", demangle(typeid(A0).name()));
-		ERROR();
-	}
-
-	return map_t<T, R>(upstream, apply);
-}
-
-
-template <typename T>
-class filter_t : public pipe_t<T, T> {
-	using filter_test = std::function<bool(T)>;
-
-	filter_test  m_test;
-	bool         m_has_value;
-	T            m_value;
-
-	void close_impl() override {}
-	bool has_next_impl() override {
-		for(;;) {
-			if (m_has_value) break;
-			if (!pipe_t<T, T>::m_upstream->has_next()) break;
-			m_value = pipe_t<T, T>::m_upstream->next();
-			m_has_value = m_test(m_value);
+		template <typename U>
+		impl_t(const U& u) : m_base(std::make_shared<wrapper_t<U>>(u)) {
+			logger.info("impl_t constructor %s", demangle(typeid(U).name()));
 		}
-		return m_has_value;
+
+		void close() override {
+			m_base->close();
+		}
+		bool has_next() override {
+			return m_base->has_next();
+		}
+		T next() override {
+			return m_base->next();
+		}
+
+		std::shared_ptr<base_t> m_base;
+	};
+
+	impl_t      m_impl;
+	std::string m_name;
+	bool        m_closed = false;
+public:
+	pipe_t(impl_t impl, const char* name) : m_impl(impl),  m_name(name) {}
+	~pipe_t() {
+		close();
 	}
-	T next_impl() override {
-		if (has_next_impl()) {
-			m_has_value = false;
-			return m_value;
+
+	void close() override {
+		if (!m_closed) {
+			m_closed = true;
+			m_impl.close();
+		}
+	}
+	bool has_next() override {
+		if (m_closed) {
+			return false;
+		} else {
+			return m_impl.has_next();
+		}
+	}
+	R next() override {
+		if (has_next()) {
+			return m_impl.next();
 		} else {
 			// if there is no next and call next(), it is error
-			logger.error("stream has no next");
-			ERROR();
+			assert(false);
 		}
 	}
-
-public:
-	filter_t(source_t<T>* upstream, filter_test test) :
-		pipe_t<T, T>(__func__, upstream),
-		m_test(test),
-		m_has_value(false) {}
-	~filter_t() {
-		base_t::close();
-	}
 };
-template <typename T, typename Predicate>
-filter_t<T>	filter(source_t<T>* upstream, Predicate test) {
-	using trait = trait_function<Predicate>;
-	using R   = typename trait::ret_type;
-	using A0_ = typename std::tuple_element<0, typename trait::arg_type>;
-	using A0  = typename A0_::type;
-
-	//logger.debug("filter T=%s R=%s Predicater=%s", demangle(typeid(T).name()), demangle(typeid(R).name()), demangle(typeid(Predicate).name()));
-	static_assert(trait::arity == 1);
-	static_assert(std::is_same_v<T, A0>);
-	static_assert(std::is_same_v<R, bool>);
-
-	// assert T     == A0T
-	// assert R     == bool
-	// assert arity == 1
-	if constexpr (!std::is_same_v<T, A0> || !std::is_same_v<R, bool> || !(trait::arity == 1)) {
-		logger.error("ASSERTION FAILED");
-		logger.error("function %s", __func__);
-		logger.error("arity %d", trait::arity);
-		logger.error("R     %s", demangle(typeid(R).name()));
-		logger.error("T     %s", demangle(typeid(T).name()));
-		logger.error("A0    %s", demangle(typeid(A0_).name()));
-		assert(false);
-	}
-
-	return filter_t<T>(upstream, test);
-}
 
 
-template <typename T>
-class count_t : public source_t<T> {
-	source_t<T>* upstream;
-	int          count;
-
-	void close_impl() override {}
-	bool has_next_impl() override {
-		return upstream->has_next();
-	}
-	T next_impl() override {
-		count++;
-		return upstream->next();
-	}
-
-public:
-	count_t(source_t<T>* upstream_, const char* name_) :
-		source_t<T>(name_),
-		upstream(upstream_),
-		count(false) {}
-	~count_t() {
-		base_t::close();
-		logger.info("count_t %s %d", base_t::name(), count);
-	}
-};
-template <typename T>
-count_t<T>	count(source_t<T>* upstream, const char* name) {
-	return count_t<T>(upstream, name);
-}
-
-
+// end of namespace stream
 }
---
+++ src/json/stream_util.h
@@ -0,0 +1,142 @@
+//
+//
+//
+
+#pragma once
+
+#include <string>
+
+#include "../util/Util.h"
+
+#include "stream.h"
+
+namespace stream {
+// begin of namespace stream
+
+
+//
+// source  vector
+//
+template <typename T>
+struct vector_impl_t : source_base_t<T> {
+	std::vector<T> m_data;
+	std::size_t    m_pos    = 0;
+
+	void close() override {}
+	bool has_next() override {
+		return 0 <= m_pos && m_pos < m_data.size();
+	}
+	T next() override {
+		return m_data[m_pos++];
+	}
+
+	vector_impl_t(std::vector<T>& data) : m_data(data) {}
+	vector_impl_t(std::initializer_list<T> init) :m_data(init.begin(), init.end()) {}
+};
+template <typename T>
+source_t<T> vector(std::initializer_list<T> init) {
+	vector_impl_t<T> impl(init);
+	return source_t<T>(impl, __func__);
+}
+template <typename T>
+source_t<T> vector(std::vector<T>& data) {
+	vector_impl_t<T> impl(data);
+	return source_t<T>(impl, __func__);
+}
+
+
+//
+// sink  sum
+//
+template <typename T, typename R>
+struct sum_impl_t : public sink_t<T, R>::base_t {
+	R m_sum = 0;
+
+	void close() override {}
+	void accept(T& newValue) override {
+		m_sum += newValue;
+	}
+	R result() override {
+		return m_sum;
+	}
+};
+template<typename T, typename R=T>
+sink_t<T, R> sum(source_base_t<T>* upstream) {
+	sum_impl_t<T, R> impl;
+	return sink_t<T, R>(impl, __func__, upstream);
+}
+
+
+//
+// sink  count
+//
+template <typename T, typename R>
+struct count_impl_t : public sink_t<T, R>::base_t {
+	R m_count = 0;
+
+	void close() override {}
+	void accept(T& newValue) override {
+		(void)newValue;
+		m_count++;
+	}
+	R result() override {
+		return m_count;
+	}
+};
+template<typename T, typename R=T>
+sink_t<T, R> count(source_base_t<T>* upstream) {
+	count_impl_t<T, R> impl;
+	return sink_t<T, R>(impl, __func__, upstream);
+}
+
+
+// pipe map
+template <typename T, typename R>
+struct map_impl_t : public source_base_t<R> {
+	using upstream_t = source_base_t<T>;
+	using map_apply  = std::function<R(T)>;
+
+	upstream_t* m_upstream;
+	map_apply   m_apply;
+
+	map_impl_t(upstream_t* upstream_, map_apply apply_) : m_upstream(upstream_), m_apply(apply_) {}
+
+	void close() override {}
+	bool has_next() override {
+		return m_upstream->has_next();
+	}
+	R next() override {
+		T newValue = m_upstream->next();
+		return m_apply(newValue);
+	}
+};
+template <typename T, typename Function>
+auto map(source_base_t<T>* upstream,  Function apply) {
+	using trait = trait_function<Function>;
+	using R   = typename trait::ret_type;
+	using A0_ = typename std::tuple_element<0, typename trait::arg_type>;
+	using A0  = typename A0_::type;
+
+	static_assert(trait::arity == 1);
+	static_assert(std::is_same_v<T, A0>);
+
+	// assert T     == A0T
+	// assert arity == 1
+//	if constexpr (!std::is_same_v<T, A0> || !(trait::arity == 1)) {
+//		logger.error("ASSERTION FAILED");
+//		logger.error("function %s", __func__);
+//		logger.error("arity %d", trait::arity);
+//		logger.error("R     %s", demangle(typeid(R).name()));
+//		logger.error("T     %s", demangle(typeid(T).name()));
+//		logger.error("A0    %s", demangle(typeid(A0).name()));
+//		ERROR();
+//	}
+
+	map_impl_t<T, R> impl(upstream, apply);
+	return pipe_t<T, R>(impl, __func__);
+}
+
+
+
+// end of namespace stream
+}

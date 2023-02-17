6570553e00e8e121b0f7c51cb6e7466304d620cc
hasegawa <hasegawa@macmini2020.lan>
2023-02-17 09:30:02 +0900

    Remove wrapper.

--- src/json/stream.h
+++ src/json/stream.h
@@ -29,39 +29,7 @@ template <typename T>
 class source_t : public source_base_t<T> {
 private:
 	using base_t = source_base_t<T>;
-	struct impl_t : public base_t {
-		template<typename U>
-		struct wrapper_t : public base_t {
-			wrapper_t(std::shared_ptr<U> u) : m_object(u) {}
-
-			void close() override {
-				m_object->close();
-			}
-			bool has_next() override {
-				return m_object->has_next();
-			}
-			T    next() override {
-				return m_object->next();
-			}
-
-			std::shared_ptr<U> m_object;
-		};
-
-		template <typename U>
-		impl_t(std::shared_ptr<U> u) : m_base(std::make_shared<wrapper_t<U>>(u)) {}
-
-		void close() override {
-			m_base->close();
-		}
-		bool has_next() override {
-			return m_base->has_next();
-		}
-		T next() override {
-			return m_base->next();
-		}
-
-		std::shared_ptr<base_t> m_base;
-	};
+	using impl_t = std::shared_ptr<base_t>;
 
 	impl_t      m_impl;
 	std::string m_name;
@@ -78,19 +46,19 @@ public:
 	void close() override {
 		if (!m_closed) {
 			m_closed = true;
-			m_impl.close();
+			m_impl->close();
 		}
 	}
 	bool has_next() override {
 		if (m_closed) {
 			return false;
 		} else {
-			return m_impl.has_next();
+			return m_impl->has_next();
 		}
 	}
 	T next() override {
 		if (has_next()) {
-			return m_impl.next();
+			return m_impl->next();
 		} else {
 			// if there is no next and call next(), it is error
 			assert(false);
@@ -114,41 +82,9 @@ public:
 		virtual void accept(T& t) = 0;
 		virtual R    result()     = 0;
 	};
+	using impl_t = std::shared_ptr<base_t>;
+
 private:
-	struct impl_t : public base_t {
-		template<typename U>
-		struct wrapper_t : public base_t {
-			wrapper_t(std::shared_ptr<U> u) : m_object(u) {}
-
-			void close() override {
-				m_object->close();
-			}
-			void accept(T& t) override {
-				m_object->accept(t);
-			}
-			R    result() override {
-				return m_object->result();
-			}
-
-			std::shared_ptr<U> m_object;
-		};
-
-		template <typename U>
-		impl_t(std::shared_ptr<U> u) : m_base(std::make_shared<wrapper_t<U>>(u)) {}
-
-		void close() override {
-			m_base->close();
-		}
-		void accept(T& t) override {
-			m_base->accept(t);
-		}
-		R result() override {
-			return m_base->result();
-		}
-
-		std::shared_ptr<base_t> m_base;
-	};
-
 	impl_t       m_impl;
 	std::string  m_name;
 	upstream_t*  m_upstream;
@@ -165,15 +101,15 @@ public:
 	void close() {
 		if (!m_closed) {
 			m_closed = true;
-			m_impl.close();
+			m_impl->close();
 		}
 	}
 	R result() {
-		return m_impl.result();
+		return m_impl->result();
 	}
 
 	void accept(T& newValue) {
-		m_impl.accept(newValue);
+		m_impl->accept(newValue);
 	}
 
 	R process() {

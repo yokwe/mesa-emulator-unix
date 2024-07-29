be4144abadb3976cdaf63b79de454672775285bf
hasegawa <hasegawa@macstudio2023.lan>
2024-07-29 17:58:04 +0900

    add testMemory.cpp

--- src2/test/testOpcode_000.cpp
+++ src2/test/testOpcode_000.cpp
@@ -28,7 +28,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************/
 
-
 //
 // tetOpcode_000.cpp
 //

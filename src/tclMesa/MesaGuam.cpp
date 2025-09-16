0f9b2a58d4d084a108d5b280fb19fb288e4247f2
yokwe <hasegawa.yasuhiro@gmail.com>
2025-09-16 22:08:03 +0900

    Add new tcl command mesa::guam display photo-image-name width height mesa::guam test display r g b

--- src/tclMesa/MesaGuam.cpp
+++ src/tclMesa/MesaGuam.cpp
@@ -33,6 +33,7 @@
 // MasaLog.cpp
 //
 
+#include <cstring>
 #include <string>
 #include <map>
 #include <utility>
@@ -41,6 +42,7 @@
 #include <tcl.h>
 #include <tclDecls.h>
 #include <tk.h>
+#include <tkDecls.h>
 
 #include "../util/Util.h"
 static const Logger logger(__FILE__);
@@ -78,6 +80,45 @@ std::map<std::string, int*> intMap = {
     FIELD_MAP_ENTRY(rmBits),
 };
 
+static std::string        imageName;
+static Tk_PhotoHandle     photoHandle = 0;
+static Tk_PhotoImageBlock imageBlock = {0, 0, 0, 0, 0, {0}};
+void initialize(Tk_PhotoImageBlock& imageBlock, int width, int height) {
+    if (imageBlock.pixelPtr) {
+        ckfree(imageBlock.pixelPtr);
+    }
+    imageBlock.width     = width;
+    imageBlock.height    = height;
+    imageBlock.pixelSize = 4; // red + gree + blue + alpha
+    imageBlock.offset[0] = 0; // red
+    imageBlock.offset[1] = 1; // green
+    imageBlock.offset[2] = 2; // blue
+    imageBlock.offset[3] = 3; // alpah
+    // use 32 bit memory access
+    int lineWidth = width + (width % 32);
+    imageBlock.pitch     = lineWidth * imageBlock.pixelSize;
+    int totalByte = imageBlock.pitch * imageBlock.height;
+    imageBlock.pixelPtr  = (unsigned char*)ckalloc(totalByte);
+    // fill with white and full opaque
+    memset(imageBlock.pixelPtr, 0xFF, totalByte);
+}
+int put(Tcl_Interp* interp, Tk_PhotoImageBlock& imageBlock) {
+    return Tk_PhotoPutBlock(interp, photoHandle, &imageBlock, 0, 0, imageBlock.width, imageBlock.height, TK_PHOTO_COMPOSITE_SET);
+}
+void fill(Tk_PhotoImageBlock& imageBlock, uint8_t r, uint8_t g, uint8_t b) {
+    uint8_t* lineStart = imageBlock.pixelPtr;
+    for(int line = 0; line < imageBlock.height; line++) {
+        auto p = lineStart;
+        for(int bit = 0; bit < imageBlock.width; bit++) {
+            p[0] = r;
+            p[1] = g;
+            p[2] = b;
+            p += imageBlock.pixelSize;
+        }
+        lineStart += imageBlock.pitch;
+    }
+}
+
 int MesaGuam(ClientData cdata, Tcl_Interp *interp_, int objc, Tcl_Obj *const objv[]) {
     (void)cdata;
     tcl::Interp interp(interp_);
@@ -85,7 +126,8 @@ int MesaGuam(ClientData cdata, Tcl_Interp *interp_, int objc, Tcl_Obj *const obj
     std::string command = tcl::toString(objv[0]);
     // mesa::guam config diskFile XXX
     // 0          1      2        3
-    //            command subjet  value
+    //            subCommand
+    //                   subject  value
     std::string subCommand = tcl::toString(objv[1]);
     if (subCommand == "config") {
         if (objc == 2) {
@@ -193,6 +235,44 @@ int MesaGuam(ClientData cdata, Tcl_Interp *interp_, int objc, Tcl_Obj *const obj
         PERF_LOG();
         memory::cache::stats();
         return TCL_OK;
+    }
+    if (subCommand == "display") {
+        if (objc == 5) {
+            // mesa::guam display imageName width height
+            // 0          1       2         3     4
+            int status;
+            imageName = tcl::toString(objv[2]);
+            auto width = toInt(interp, objv[3], status);
+            if (status != TCL_OK) return status;
+            auto height = toInt(interp, objv[4], status);
+            if (status != TCL_OK) return status;
+
+            initialize(imageBlock, width, height);
+            fill(imageBlock, 255, 255, 255);
+
+            photoHandle = Tk_FindPhoto(interp, imageName.c_str());
+            put(interp, imageBlock);
+            return TCL_OK;
+        }
+    }
+    if (subCommand == "test") {
+        if (objc == 6) {
+            std::string subject = tcl::toString(objv[2]);
+            // mesa::guam test display r g b
+            // 0          1    2       3 4 5
+            if (subject == "display") {
+                int status;
+                auto r = toInt(interp, objv[3], status);
+                if (status != TCL_OK) return status;
+                auto g = toInt(interp, objv[4], status);
+                if (status != TCL_OK) return status;
+                auto b = toInt(interp, objv[5], status);
+                if (status != TCL_OK) return status;
+                fill(imageBlock, r, g, b);
+                put(interp, imageBlock);
+                return TCL_OK;
+            }
+        }
     }
 
     {

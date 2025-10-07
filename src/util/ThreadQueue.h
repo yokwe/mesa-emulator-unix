545ec5b126b473a329066057ae22960b86becabe
yokwe <hasegawa.yasuhiro@gmail.com>
2025-10-07 11:06:47 +0900

    Remove unused method

--- src/util/ThreadQueue.h
+++ src/util/ThreadQueue.h
@@ -1,14 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2025, Yasuhiro Hasegawa
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+
 //
+// ThreadQueue.h
 //
-//
+
+#pragma once
 
 #include <chrono>
 #include <deque>
 #include <mutex>
 
-
 #include "../util/Util.h"
-
 
 
 namespace thread_queue {
@@ -68,17 +98,12 @@ public:
     ThreadQueueProducer() : stopThread(false) {}
 
     // produce return true when data has value
-    virtual bool produce(T& data, std::chrono::milliseconds duration) = 0;
+    virtual bool produce(T& data, std::chrono::milliseconds timeout) = 0;
 
     void stop() {
         stopThread = true;
     }
-
-    bool empty() {
-        std::unique_lock<std::mutex> lock(mutex);
-        return queue.empty();
-    }
-
+ 
     // pop return true when data has value
     bool pop(T& data, std::chrono::milliseconds duration = Util::ONE_SECOND) {
         std::unique_lock<std::mutex> lock(mutex);

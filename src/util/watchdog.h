e8315c77806fb45e9e79799d8c184bf82ebc3be4
yokwe <hasegawa.yasuhiro@gmail.com>
2025-10-30 15:44:19 +0900

    Minor modification Use std::chrono::system_clock::time_point for updateTime

--- src/util/watchdog.h
+++ src/util/watchdog.h
@@ -34,7 +34,7 @@
 
 #pragma once
 
-#include <atomic>
+#include <chrono>
 #include <functional>
 
 namespace watchdog {
@@ -48,18 +48,19 @@ void insert(Watchdog* watchdog);
 void remove(Watchdog* watchdog);
 
 struct Watchdog {
-    const char*           name;
-    uint64_t              threshold;
-    std::function<void()> action;
-    std::atomic_uint64_t  updateTime;
+    const char*               name;
+    std::chrono::milliseconds threshold;
+    std::function<void()>     action;
 
-    Watchdog(const char* name_, uint64_t threshold_, std::function<void()> action_) : name(name_), threshold(threshold_), action(action_) {
+    std::chrono::system_clock::time_point updateTime;
+    
+    Watchdog(const char* name_, std::chrono::milliseconds threshold_, std::function<void()> action_) :
+        name(name_), threshold(threshold_), action(action_) {
         update();
     }
 
     void update() {
-        auto duration = std::chrono::system_clock::now().time_since_epoch();
-        updateTime = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+        updateTime = std::chrono::system_clock::now();
     }
 };
 

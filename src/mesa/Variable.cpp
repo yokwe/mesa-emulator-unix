4d2e68a935ca76b4500eb989b9b735447f82eb13
yokwe <hasegawa.yasuhiro@gmail.com>
2026-01-19 11:51:00 +0900

    Minor modification Display MP code with meaning of code

--- src/mesa/Variable.cpp
+++ src/mesa/Variable.cpp
@@ -33,7 +33,7 @@
 // Variable.cpp
 //
 
-//#include <map>
+#include <unordered_map>
 
 #include "Variable.h"
 
@@ -213,8 +213,102 @@ void variable::dump() {
 //
 // VariableMP
 //
+static std::unordered_map<CARD16, const char*> mp_message_map = {
+    {900, "Germ entered"},
+    {901, "Germ out of frames (Pilot bug)"},
+    {902, "unexpected trap or kernel function call (Pilot bug)"},
+    {903, "attempt to start an already started module (Pilot bug)"},
+    {904, "page or write protect fault encountered (Pilot bug)"},
+    {905, "Germ not compatible with initial microcode"},
+    {906, "Germ and running Pilot have different version numbers"},
+    {907, "reschedule error, typically because of page or frame fault (Pilot bug)"},
+    {909, "Germ SIGNAL or ERROR (Pilot bug)"},
+    {910, "Germ action running (e.g. inLoad, outLoad)"},
+    {911, "Germ and physical volume have incompatible version numbers"},
+    {912, "Germ and boot file have incompatible version numbers"},
+    {913, "no physical boot file installed"},
+    {914, "boot file contains invalid data"},
+    {915, "waiting for ethernet debugger to begin debugging me"},
+    {916, "boot file won't fit in real memory"},
+    {917, "talking to ethernet debugger"},
+    {919, "Germ transferred control back to caller (who has hung)"},
+    {920, "Germ driver running (e.g. disk, ether, floppy)"},
+    {921, "hard error on device being booted"},
+    {922, "operation on boot device no completed in expected time"},
+    {923, "broken link in chained boot file (try reinstalling)"},
+    {924, "no response to Germ's request for ether boot file"},
+    {925, "e.g. unexpected sequence number or size"},
+    {926, "booting media needs attention, e.g., retentioning"},
+    {927, "boot file ends before it should (try reinstalling)"},
+    {928, "waiting for any boot server to respond"},
+    {929, "expected descriptor page doesn't look like one (try reinstalling)"},
+    {930, "Pilot Control and MesaRuntime components being initialized"},
+    {931, "Pilot and StartPilot have incompatible version numbers"},
+    {932, "runtime trap before appropriate trap handler set up (Pilot bug)"},
+    {933, "Pilot and Germ have incompatible version numbers"},
+    {934, "boot file's StartList contains bad data"},
+    {935, "need ethernet debugee server but boot loader being used does not have that capability."},
+    {936, "waiting for microcode debugger"},
+    {937, "trying to get the time from either hardware clock or ethernet"},
+    {938, "running cleanup procedures, e.g. before going to debugger"},
+    {939, "ProcessorFace.PowerOff called but no power control relay"},
+    {940, "Pilot Store component being initialized"},
+    {941, "Bad LoadState version"},
+    {946, "system logical volume needs scavenging[riskyRepair]"},
+    {947, "waiting for disk drive to become ready"},
+    {948, "system physical volume needs scavenging"},
+    {949, "disk hardware error while scavenging system volume"},
+    {950, "logical volume being scavenged"},
+    {951, "alternate feedback for progress during a pass of logical volume scavenging"},
+    {952, "alternate feedback for additional passes during logical volume scavenging"},
+    {953, "debugger pointers have been set to a nonexistent volume or to a volume without an installed debugger"},
+    {960, "temporary files from previous run being deleted"},
+    {965, "insufficient file space for data space backing storage (specify smaller size with boot switch)"},
+    {966, "insufficient file space for file lock nodes"},
+    {970, "client and other non-bootloaded code being mapped"},
+    {980, "Pilot Communication component being initialized"},
+    {981, "trying to find a Pup / EthernetOne 8 bit address"},
+    {982, "can't determine ARPA 32 bit host address"},
+    {990, "PilotClient.Run called"},
+	// ViewPoint Application Developers Guide
+	// C.1 ViewPoint Savenger Maintenace Panel Codes
+	{7500, "Scavenge in progress. No action required"},
+	{7501, "User volume requires scavenging . Press the F and C keys to proceed"},
+	{7502, "Appears afer you press F and C from 7501, should to to 7500 when you release F and C"},
+	{7504, "Volume needs initializing. Press and release I and V keys"},
+	{7508, "Volume needs converting Press and release F and C keys to proceed"},
+	{7511, "There is no scavenger boot file on the Scavenger, System or Star volume"},
+	// C.1.1 Normal MP Codes
+	{7920, "Start ViewPoint"},
+	{7940, "Start invisible application folders"},
+	{7960, "Start visible application folder with autorun = TRUE"},
+	{8000, "Done booting"},
+
+	{7600, "Start ViewPoint"},
+	{7700, "Start invisible application folders"},
+	{7800, "Start visible application folder with autorun = TRUE"},
+
+	// C1.2 Debugger Substitute MP Codes
+	{7540, "AddressFault"},
+	{7541, "BreadPoint"},
+	{7542, "Bug"},
+	{7543, "CallDebugger"},
+	{7544, "CleanMapLog"},
+	{7545, "Diskerror"},
+	{7546, "Interrupt"},
+	{7547, "Return"},
+	{7548, "ReturnAborted"},
+	{7549, "UncaughtSignal"},
+	{7550, "VisitDebugger"},
+	{7551, "WriteProtectFault"},
+	{7552, "Other"},
+};
 static void mp_message(CARD16 mp) {
-    logger.info("MP %04d", mp);
+	if (mp_message_map.contains(mp)) {
+		logger.info("MP %04d  %s", mp, mp_message_map.at(mp));
+	} else {
+		logger.info("MP %04d", mp);
+	}
 } 
 void VariableMP::initialize() {
 	addObserver(mp_message);
